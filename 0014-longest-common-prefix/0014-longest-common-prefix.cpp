8aab20a0ae8cc324a1944b3dea5127374e0684ca
dharani2816 <sarandharan514@gmail.com>
2025-09-05 13:07:04 +0530

    Time: 0 ms (100%), Space: 11.9 MB (49.27%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.cpp
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -1,44 +1,17 @@
 class Solution {
 public:
     string longestCommonPrefix(vector<string>& strs) {
-        /*  sort(strs.begin(), strs.end(), [](const string &a, const string &b)
-   { return a.size() < b.size();
-   });
-       string result = "";
-       int n = strs.size();
-       int m = strs[0].size();
-       int k = 0;
-       int flag;
-       for (int i = 0; i < m; i++) {
-           flag = 1;
-           char a = strs[0][i];
-           for (int j = 1; j < n; j++) {
-               if (a == strs[j][i]) {
-                   flag++;
-               }
-           }
-           if (flag == n) {
-               result[k] = a;
-               k++;
-           }
-       }
-       return result;*/
-        sort(strs.begin(), strs.end());
+        sort(strs.begin(), strs.end());//sort lexicographically
         string result = "";
-        int m = strs[0].size();
-        int n = strs.size();
+        int m = strs[0].size();//length of string with least size
+        int n = strs.size();//no of strings
         for (int i = 0; i < m; i++) {
             if (strs[0][i] == strs[n - 1][i]) {
                 result.push_back(strs[0][i]);
-            }
-            else{
+            } else {
                 break;
             }
         }
-        /*if(result[0] == ' '){
-            return "";
-        }
-         result.erase(remove(result.begin(), result.end(), ' '), result.end());*/
         return result;
     }
 };
