bcfd95a978b48b48d15899976bf68fdab50f6c19
dharani2816 <2024503528@student.annauniv.edu>
2025-10-21 12:20:49 +0530

    Time: 2 ms (49.95%), Space: 45.2 MB (16%) - LeetHub

--- 2942-find-words-containing-character/2942-find-words-containing-character.java
+++ 2942-find-words-containing-character/2942-find-words-containing-character.java
@@ -1,9 +1,10 @@
 class Solution {
     public List<Integer> findWordsContaining(String[] words, char x) {
         ArrayList<Integer> arr = new ArrayList<>();
+        String val = String.valueOf(x);
         int i=0;
         for(String str : words){
-            if(str.contains(String.valueOf(x))) arr.add(i);
+            if(str.contains(val)) arr.add(i);
             i++;
         }
         return arr;
