f917c953900a294e0f7b9dcc54ab35c1410c8704
dharani2816 <2024503528@student.annauniv.edu>
2026-01-26 20:22:36 +0530

    Time: 0 ms (100%), Space: 28.2 MB (62.5%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,22 +1,30 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-       sort(nums.begin(),nums.end());
-       int maxCount = 0;
-       int n= nums.size();
-       int element;
-       for(int i=0;i<nums.size();i++){
-        maxCount++;
-        if(maxCount > n /2){
-            element = nums[i];
+    //    sort(nums.begin(),nums.end());
+    //    int maxCount = 0;
+  int n= nums.size();
+    //    int element;
+    //    for(int i=0;i<nums.size();i++){
+    //     maxCount++;
+    //     if(maxCount > n /2){
+    //         element = nums[i];
+    //     }
+    //     if(i!=n-1 && nums[i] != nums[i+1]){
+    //         maxCount = 0;
+    //     }
+    //    }
+    //    if(maxCount > n /2){
+    //         element = nums[n-1];
+    //     }
+    //    return element;
+    unordered_map<int,int>freq;
+    for(int i=0;i<nums.size();i++){
+        freq[nums[i]]++;
+        if(freq[nums[i]] > n/2){
+            return nums[i];
         }
-        if(i!=n-1 && nums[i] != nums[i+1]){
-            maxCount = 0;
-        }
-       }
-       if(maxCount > n /2){
-            element = nums[n-1];
-        }
-       return element;
+    }
+    return -1;
     }
 };
