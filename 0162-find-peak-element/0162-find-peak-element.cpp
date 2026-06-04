400aea5b65b3f3b063dd11b37ff11581cc807c3e
dharani2816 <2024503528@student.annauniv.edu>
2026-06-04 15:31:37 +0530

    Time: 0 ms (100%), Space: 12.7 MB (5.46%) - LeetHub

---
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    int findPeakElement(vector<int>& nums) {
+        //check whether the current mid is in increasing part of curve or decreasing part of curve assuming the array has one peak
+        int n = nums.size();
+        if(n == 1){
+            return 0;
+        }
+        else{
+            if(nums[0] > nums[1]){
+                return 0;
+            }
+            else if(nums[n-1] > nums[n-2]){
+                return n-1;
+            }
+            else{
+                int low = 1;
+                int high = n-2;
+                while(low <= high){
+                    int mid = (low+high)/2;
+                    if(nums[mid] > nums[mid-1] && nums[mid] > nums[mid+1]){
+                        return mid;
+                    }
+                    else if(nums[mid] > nums[mid-1]){
+                        low = mid+1;
+                    }
+                    else if(nums[mid] > nums[mid+1]){
+                        high = mid-1;
+                    }
+                    else{
+                        low = mid +1;
+                    }
+                }
+                return -1;
+            }
+        }
+    }
+};
