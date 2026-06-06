6d7a52d8cb720e52c9acd90c47a6c562c0574c3b
dharani2816 <2024503528@student.annauniv.edu>
2026-06-06 21:25:21 +0530

    Time: 0 ms (100%), Space: 9.8 MB (48.8%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,19 +1,22 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        unordered_map<char, int> count;
-        unordered_map<char, int> count1;
+        if (s.size() != t.size()) {
+            return false;
+        }
+        unordered_map<char, int> freq;
         for (int i = 0; i < s.size(); i++) {
-            count[s[i]]++;
+            freq[s[i]]++;
         }
         for (int i = 0; i < t.size(); i++) {
-            count1[t[i]]++;
+            if (freq[t[i]]) {
+                freq[t[i]]--;
+            } else {
+                return false;
+            }
         }
-        string r = s + t;
-        unordered_set<char> s1(r.begin(), r.end());
-
-        for (auto i : s1) {
-            if (count[i] != count1[i]) {
+        for (int i = 0; i < t.size(); i++) {
+            if (freq[t[i]] != 0) {
                 return false;
             }
         }
