ad8cb50c55d9ae5a439e205748167b0566c189b5
dharani2816 <2024503528@student.annauniv.edu>
2026-06-08 16:09:41 +0530

    Time: 0 ms (100%), Space: 19.8 MB (8.26%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
@@ -10,35 +10,38 @@
  */
 class Solution {
 public:
+    void insert(ListNode*& head, ListNode*& n, int num) {
+        ListNode* nn = new ListNode(num);
+        if (!head) {
+            head = nn;
+            n = nn;
+        } else {
+            n->next = nn;
+            n = nn;
+        }
+    }
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        ListNode* temp1 = list1;
-        ListNode* temp2 = list2;
-        ListNode* list3;
-        vector<int> ans;
-        int i = 0;
-        while (temp1 != nullptr) {
-            ans.push_back(temp1->val);
-            temp1 = temp1->next;
-        }
-        while (temp2 != nullptr) {
-            ans.push_back(temp2->val);
-            temp2 = temp2->next;
-        }
-        sort(ans.begin(), ans.end());
-        for (int i = 0; i < ans.size(); i++) {
-            if( i == 0){
-                ListNode* temp = new ListNode(ans[i]);
-                list3 = temp;
-            }
-            else{
-                ListNode *temp1 = new ListNode(ans[i]);;
-                ListNode* temp2 = list3;
-                while(temp2->next != nullptr){
-                    temp2 = temp2->next;
-                }
-                temp2->next = temp1;
+        ListNode* l1 = list1;
+        ListNode* l2 = list2;
+        ListNode* resultHead = nullptr;
+        ListNode* result = nullptr;
+        while (l1 && l2) {
+            if (l1->val < l2->val) {
+                insert(resultHead, result, l1->val);
+                l1 = l1->next;
+            } else {
+                insert(resultHead, result, l2->val);
+                l2 = l2->next;
             }
         }
-        return list3;
+        while (l1) {
+            insert(resultHead, result, l1->val);
+            l1 = l1->next;
+        }
+        while (l2) {
+            insert(resultHead, result, l2->val);
+            l2 = l2->next;
+        }
+        return resultHead;
     }
 };
