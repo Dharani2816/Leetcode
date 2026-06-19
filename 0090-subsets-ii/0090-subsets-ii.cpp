bddcaf524a440c4e7657d2bca4bb26148d4fca85
dharani2816 <2024503528@student.annauniv.edu>
2026-06-19 11:17:40 +0530

    Time: 7 ms (6.45%), Space: 17.8 MB (5.2%) - LeetHub

---
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    void generate(int index,int n,vector<int>&nums,vector<int>a,vector<vector<int>>&res){
+        if(index == n){
+            res.push_back(a);
+            return;
+        }
+        a.push_back(nums[index]);
+        generate(index+1,n,nums,a,res);
+        a.pop_back();
+        generate(index+1,n,nums,a,res);
+    }
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+         vector<vector<int>>result;
+       vector<int>a;
+       sort(nums.begin(),nums.end());
+       generate(0,nums.size(),nums,a,result);
+       sort(result.begin(),result.end());
+       result.erase(unique(result.begin(),result.end()),result.end());
+       return result; 
+    }
+};
