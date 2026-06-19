667a4036748f3351d97b28f21b780a9bf47d4909
dharani2816 <2024503528@student.annauniv.edu>
2026-06-19 13:35:35 +0530

    Time: 0 ms (100%), Space: 25.1 MB (76.52%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.cpp
+++ 0645-set-mismatch/0645-set-mismatch.cpp
@@ -1,15 +1,18 @@
 class Solution {
 public:
     vector<int> findErrorNums(vector<int>& nums) {
-        unordered_map<int,int>freq;
-        for(int i=0;i<nums.size();i++){
-            freq[nums[i]]++;
-        }
+        sort(nums.begin(),nums.end());
         int repeated = 1,missing=1;
-       for(int i=1;i<=nums.size();i++){
-        if(freq[i] > 1) repeated = i;
-        if(freq[i] == 0) missing = i;
-       }
+        int n = nums.size();
+        for(int i=0;i<n-1;i++){
+            if(nums[i+1] - nums[i] == 0){
+                repeated = nums[i];
+            }
+            else if(nums[i+1] - nums[i] > 1){
+                missing = nums[i]+1;
+            }
+        }
+        if(n != nums[n-1]) missing = n;
        return {repeated,missing};
     }
 };
