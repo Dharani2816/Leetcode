dbf0a6a3ed23c77ad92d4396da23b71f6dbda9b1
dharani2816 <2024503528@student.annauniv.edu>
2026-06-20 14:51:26 +0530

    Time: 0 ms (100%), Space: 8.3 MB (45.44%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.cpp
+++ 0190-reverse-bits/0190-reverse-bits.cpp
@@ -3,7 +3,7 @@ public:
     int reverseBits(int n) {
         int sum = 0;
         for(int i=31;i>=0;i--){
-            if(n & 1) sum+=pow(2,i);
+            if(n & 1) sum+=(1 << i);
             n = n >> 1;
         }
         return sum;
