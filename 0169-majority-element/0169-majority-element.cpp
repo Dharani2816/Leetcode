69db8af027639482cf46dfe7e59d43beae9e7bd8
dharani2816 <2024503528@student.annauniv.edu>
2026-06-21 16:26:12 +0530

    Time: 0 ms (100%), Space: 28.4 MB (7.82%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,30 +1,17 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-    //    sort(nums.begin(),nums.end());
-    //    int maxCount = 0;
-  int n= nums.size();
-    //    int element;
-    //    for(int i=0;i<nums.size();i++){
-    //     maxCount++;
-    //     if(maxCount > n /2){
-    //         element = nums[i];
-    //     }
-    //     if(i!=n-1 && nums[i] != nums[i+1]){
-    //         maxCount = 0;
-    //     }
-    //    }
-    //    if(maxCount > n /2){
-    //         element = nums[n-1];
-    //     }
-    //    return element;
-    unordered_map<int,int>freq;
-    for(int i=0;i<nums.size();i++){
-        freq[nums[i]]++;
-        if(freq[nums[i]] > n/2){
-            return nums[i];
+        //moores voting algortihm freq of element appearing more than n/2 times wont be canclled out this is the basic intuition
+        int element = nums[0],freq = 0;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i] == element) freq++;
+            else freq--;
+            if(freq == 0){
+                if(i != nums.size()-1){
+                    element = nums[i+1];
+                }
+            }
         }
-    }
-    return -1;
+        return element; //since majority always exist
     }
 };
