9034bfd88a07c4946bd1658ab2e62ac1fe7fd0ca
dharani2816 <2024503528@student.annauniv.edu>
2026-06-24 11:54:17 +0530

    Time: 55 ms (38.24%), Space: 51.1 MB (8.86%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -1,13 +1,17 @@
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
+        unordered_map<int,int>sum;
+        int prefixSum = 0;
+        sum[0]++;
         int count = 0;
         for(int i=0;i<nums.size();i++){
-            int sum = 0;
-            for(int j=i;j<nums.size();j++){
-                sum+=nums[j];
-                if(sum == k) count++;
+            prefixSum += nums[i];
+            //check if there is a subarray that has sum prefixSum - k if it is there find its count then it means that there count subarrays with sum = k that ends at index i;
+            if(sum[prefixSum - k]){
+                count+=sum[prefixSum - k];
             }
+            sum[prefixSum]++;
         }
         return count;
     }
