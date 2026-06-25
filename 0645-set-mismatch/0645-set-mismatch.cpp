702df4c99122933ea0d58fdce49096cf4e3e6849
dharani2816 <2024503528@student.annauniv.edu>
2026-06-25 11:45:59 +0530

    Time: 15 ms (32.79%), Space: 25.1 MB (88.83%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.cpp
+++ 0645-set-mismatch/0645-set-mismatch.cpp
@@ -1,18 +1,20 @@
 class Solution {
 public:
     vector<int> findErrorNums(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        int repeated = 1,missing=1;
-        int n = nums.size();
-        for(int i=0;i<n-1;i++){
-            if(nums[i+1] - nums[i] == 0){
-                repeated = nums[i];
-            }
-            else if(nums[i+1] - nums[i] > 1){
-                missing = nums[i]+1;
-            }
+        long long repeating = -1,missing = -1;
+        //can be solved using basic math (s1 = sum of  nums) - (s1n = sum of 1 to n) = x-y and (s2 = sum of square of nums) - (s2n = summ of sq of 1 to n) = x2 - y2;
+        long long n = nums.size();
+        long long s1=0,s2=0;
+        long long s1n = (n * (n+1)) / 2;
+        long long s2n = (n* (n+1) * (2*n+1))/6;
+        for(int i=0;i<nums.size();i++){
+            s1 += nums[i];
+            s2 += nums[i] * nums[i];
         }
-        if(n != nums[n-1]) missing = n;
-       return {repeated,missing};
+        long long val1 = s1 - s1n; //value of x-y
+        long long val2 = (s2 - s2n)/val1; // value of x+y x->repeating y->missing
+        repeating = (val1+val2)/2;
+        missing = repeating - val1;
+        return {(int)repeating,(int)missing};
     }
 };
