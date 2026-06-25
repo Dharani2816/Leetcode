90edfadc1e514428166d10eac6a5fe6c924c9e91
dharani2816 <2024503528@student.annauniv.edu>
2026-06-25 13:04:34 +0530

    Time: 0 ms (100%), Space: 8.8 MB (30.52%) - LeetHub

--- 1422-maximum-score-after-splitting-a-string/1422-maximum-score-after-splitting-a-string.cpp
+++ 1422-maximum-score-after-splitting-a-string/1422-maximum-score-after-splitting-a-string.cpp
@@ -8,6 +8,7 @@ public:
             if(s[i] == '1') ones++;
         }
         int score = 0;
+        //why we got till n-2 is sice we include current character in left substring we will get the entire string in n-1 th index so we get a empty right substring which is wrong
         for(int i=0;i<n-1;i++){
             if(s[i] == '0') zeros++; // since the current character comes in left string we reduce the total ones by one when we get thhe character as one
            else ones--;
