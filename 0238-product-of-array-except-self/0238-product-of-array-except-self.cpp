cb0355a6d8dff470652c24e6e19819773d870c80
dharani2816 <2024503528@student.annauniv.edu>
2026-06-27 18:03:07 +0530

    Time: 0 ms (100%), Space: 40.1 MB (80.94%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -1,53 +1,17 @@
 class Solution {
 public:
-    int getPrefixProduct(vector<int>nums,int index){
-        if(index == 0){
-            return 1;
-        }
-        else{
-            int product = 1;
-            for(int i=0;i<index;i++){
-                product *= nums[i];
-            }
-            return product;
-        }
-    }
-    int getSuffixProduct(vector<int>nums,int index){
-        if(index == nums.size()-1){
-            return 1;
-        }
-        else{
-            int product = 1;
-            for(int i=nums.size()-1;i>index;i--){
-                product *= nums[i];
-            }
-            return product;
-        }
-    }
     vector<int> productExceptSelf(vector<int>& nums) {
-        vector<int>res;
-        unordered_map<int,int>prefProd;
-        unordered_map<int,int>suffProd;
+        vector<int>result(nums.size());
+        int prefix = 1;
         for(int i=0;i<nums.size();i++){
-            if(i == 0){
-                prefProd[i] = 1;
-            }
-            else{
-                prefProd[i] = prefProd[i-1]*nums[i-1];
-            }
+            result[i] = prefix;
+            prefix *= nums[i]; 
         }
+        int suffix = 1;
         for(int i=nums.size()-1;i>=0;i--){
-            if(i == nums.size()-1){
-                suffProd[i] = 1;
-            }
-            else{
-                suffProd[i] = suffProd[i+1]*nums[i+1];
-            }
+            result[i] *= suffix;
+            suffix *= nums[i];
         }
-
-        for(int i=0;i<nums.size();i++){
-            res.push_back(prefProd[i]*suffProd[i]);
-        }
-        return res;
+        return result;
     }
 };
