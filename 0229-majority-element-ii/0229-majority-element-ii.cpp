c8c2db75dd212e81fe35090228c4ff9d80f847e0
dharani2816 <2024503528@student.annauniv.edu>
2026-06-29 16:23:23 +0530

    Time: 0 ms (100%), Space: 23.7 MB (44.06%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -1,17 +1,35 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
+        //moores voting algorithm instead of using a single candidate like in maj element 1 we use two candidates as there is possibility of two elements appearing more than n/3 times 
+        int candidate1 = INT_MIN,candidate2 = INT_MIN;
+        int count1 = 0,count2 = 0;
         int n = nums.size();
-        vector<int>result;
-        unordered_map<int, int> freq;
-        unordered_map<int,bool>isPushed;
-        for (int i = 0; i < n; i++) {
-            freq[nums[i]]++;
-            if (freq[nums[i]] > n / 3 && !isPushed[nums[i]]) {
-                result.push_back(nums[i]);
-                isPushed[nums[i]] = true;
+        for(int i=0;i<n;i++){
+            if(count1 == 0 && nums[i]!=candidate2){
+                candidate1 = nums[i]; count1 = 1;continue;
             }
+            else if(count2 == 0 && nums[i]!=candidate1){
+                candidate2 = nums[i]; count2 = 1;continue;
+            }
+            if(nums[i] == candidate1){
+                count1++;
+            }
+            else if(nums[i] == candidate2){
+                count2++;
+            }
+            else {
+                count1--;count2--;
+            }
+            
         }
+        vector<int>result; int freq1 = 0,freq2 = 0;
+        for(int i=0;i<n;i++){
+            if(nums[i] == candidate1) freq1++;
+            if(nums[i] == candidate2) freq2++;
+        }
+        if(freq1 > (n/3)) result.push_back(candidate1);
+        if(freq2 > (n/3)) result.push_back(candidate2);
         return result;
     }
 };
