20ba10cf7c6230fa77f363efd9ba61cbd208e318
dharani2816 <2024503528@student.annauniv.edu>
2026-08-26 23:32:15 +0530

    Time: 0 ms (100%), Space: 9.3 MB (77.34%) - LeetHub

---
+++ 0059-spiral-matrix-ii/0059-spiral-matrix-ii.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    vector<vector<int>> generateMatrix(int n) {
+        int top = 0;
+        int bottom = n-1;
+        int left = 0;
+        int right = n-1;
+        int number = 1;
+        vector<vector<int>>grid(n,vector<int>(n,0));
+        while(top<=bottom && left<=right){
+            for(int i=left;i<=right;i++){
+                grid[top][i] = number;
+                number++;
+            }
+            top++;
+            for(int i=top;i<=bottom;i++){
+                grid[i][right] = number;
+                number++;
+            }
+            right--;
+            if(top<=bottom){
+                for(int i=right;i>=left;i--){
+                    grid[bottom][i] = number;
+                    number++;
+                }
+                bottom--;
+            }
+            if(left<=right){
+                for(int i=bottom;i>=top;i--){
+                    grid[i][left] = number;
+                    number++;
+                }
+                left++;
+            }
+        }
+        return grid;
+    }
+};
